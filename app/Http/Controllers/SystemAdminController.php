a4224023887c3b1df304c425fd17f2ba2a7bbe14
mubarak23 <mubarakaminu340@gmail.com>
2020-04-12 02:38:15 +0100

    create controller for actors in hmo

---
+++ app/Http/Controllers/SystemAdminController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SystemAdmin;
+use Illuminate\Http\Request;
+
+class SystemAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SystemAdmin  $systemAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SystemAdmin $systemAdmin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SystemAdmin  $systemAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SystemAdmin $systemAdmin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SystemAdmin  $systemAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SystemAdmin $systemAdmin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SystemAdmin  $systemAdmin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SystemAdmin $systemAdmin)
+    {
+        //
+    }
+}
---
+++ app/SystemAdmin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SystemAdmin extends Model
+{
+    //
+}
