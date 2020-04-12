fd1a79011a70e1e020dd87cd051fd86e4f542d9d
mubarak23 <mubarakaminu340@gmail.com>
2020-04-12 03:12:43 +0100

    wired the route to specific middleware

--- app/Http/Middleware/Pharmacist.php
+++ app/Http/Middleware/Pharmacist.php
@@ -15,6 +15,37 @@ class Pharmacist
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        //return $next($request);
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->role == 1) {
+            return redirect()->route('systemadmin');
+        }
+
+        if (Auth::user()->role == 2) {
+            return redirect()->route('doctor');
+        }
+
+        if (Auth::user()->role == 3) {
+            return redirect()->route('nurse');
+        }
+
+        if (Auth::user()->role == 5) {
+            return redirect()->route('labaratory');
+        }
+
+        if (Auth::user()->role == 6) {
+            return redirect()->route('receptionist');
+        }
+
+        if (Auth::user()->role == 7) {
+            return redirect()->route('patient');
+        }
+
+        if (Auth::user()->role == 4) {
+            return $next($request);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,56 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/admin', [
+        'uses' => 'SystemAdminController@index',
+        'as' => 'admin.index'
+    ]);
+})->middleware('systemadmin');
+
+Route::group(['prefix' => 'doctor'], function(){
+    Route::get('/doctor', [
+        'uses' => 'DoctorController@index',
+        'as' => 'doctor.index'
+    ]);
+})->middleware('doctor');
+
+Route::group(['prefix' => 'nurse'], function(){
+    Route::get('/admin', [
+        'uses' => 'NurseController@index',
+        'as' => 'nurse.index'
+    ]);
+})->middleware('nurse');
+
+Route::group(['prefix' => 'receptionist'], function(){
+    Route::get('/admin', [
+        'uses' => 'ReceptionistController@index',
+        'as' => 'receptionist.index'
+    ]);
+})->middleware('receptionist');
+
+Route::group(['prefix' => 'pharmacist'], function(){
+    Route::get('/admin', [
+        'uses' => 'PharmacistController@index',
+        'as' => 'pharmacist.index'
+    ]);
+})->middleware('pharmacist');
+
+Route::group(['prefix' => 'laboratory'], function(){
+    Route::get('/admin', [
+        'uses' => 'LaboratoryController@index',
+        'as' => 'laboratory.index'
+    ]);
+})->middleware('laboratory');
+
+Route::group(['prefix' => 'patient'], function(){
+    Route::get('/admin', [
+        'uses' => 'PatientController@index',
+        'as' => 'patient.index'
+    ]);
+})->middleware('patient');
+
+
+
+
 Route::get('/home', 'HomeController@index')->name('home');
