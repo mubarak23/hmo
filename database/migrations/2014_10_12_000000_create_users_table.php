d367fdcdbc9670e4e7646edadeda802601ea6c31
mubarak23 <mubarakaminu340@gmail.com>
2020-04-15 10:06:09 +0100

    structure default admin dashboard

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('phone');
-            $table->integer('user_roles_id');
+            $table->integer('user_roles_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
