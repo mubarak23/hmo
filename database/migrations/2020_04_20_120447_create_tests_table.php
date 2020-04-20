a6da4a10cf011909f7259642def70440f9e1044e
mubarak23 <mubarakaminu340@gmail.com>
2020-04-20 13:11:57 +0100

    table for test

---
+++ database/migrations/2020_04_20_120447_create_tests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('patient_id');
+            $table->integer('doctor_id');
+            $table->string('type');
+            $table->string('lab_service')->nullable();
+            $table->boolean('status')->nullable();
+            $table->boolean('request')->nullable();
+            $table->string('result_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
